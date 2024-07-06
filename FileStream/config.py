a30959790296df43e1292289c8ba252feb342a1e
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-07-06 09:48:03 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,24 +4,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID"))
-    API_HASH = str(env.get("API_HASH"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
+    API_ID = int(env.get("API_ID", "27696177"))
+    API_HASH = str(env.get("API_HASH", "0c44906a4feff3b947db76dfa7c57d88"))
+    BOT_TOKEN = str(env.get("BOT_TOKEN", "7245904271:AAHi4-d2VAbYZgvRt-PNfFAxpyQ4NcERvTE"))
+    OWNER_ID = int(env.get('OWNER_ID', '5255176126'))
+    TO_CHANNEL = int(env.get("TO_CHANNEL", "-1002207001310"))   # Logs channel for user logs
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
-    FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
+    DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://fehebaw351:nHbjrujWqgqLR58H@cluster0.lekn97z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+    DATABASE_NAME = env.get("DATABASE_NAME", "cluste0")
+    COLLECTION_NAME = env.get('COLLECTION_NAME', 'Data')
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "TMR_update_channel"))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'store05'))
+    FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1001868502293")
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
-    SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
+    SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "10"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
-    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
+    START_PIC = env.get('START_PIC', "https://graph.org/file/e581dec86ccfdfbdbf3b9.jpg")
+    VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/e581dec86ccfdfbdbf3b9.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002207001310"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002207001310"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
@@ -32,10 +35,7 @@ class Server:
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format(
+    FQDN = str(env.get("FQDN", "ssrfs-7ecfd11f630e.herokuapp.com"))
+    URL = "https://filestrem-3ed9fe0ee6aa.herokuapp.com/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
-
-
-
